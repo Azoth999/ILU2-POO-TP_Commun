47fc517e3d082b6da7fb782c756a661fd4404816
Azoth999 <yanispeyrat@gmail.com>
2023-03-30 19:59:51 +0200

    Update ReservationSpectacle.java

--- src/model/ReservationSpectacle.java
+++ src/model/ReservationSpectacle.java
@@ -1,5 +1,21 @@
 package model;
 
 public class ReservationSpectacle extends Reservation {
-
+	private int numPlace;
+	private int numZone;
+	
+	public ReservationSpectacle(int jour, int mois, int numPlace, int numZone) {
+		super(jour, mois);
+		this.numPlace = numPlace;
+		this.numZone = numZone;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder texte = new StringBuilder();
+		texte.append("Le " + jour + "/" + mois);
+		texte.append(" : place n°" + numPlace);
+		texte.append(" zone n°" + numZone + ".");
+		return texte.toString();
+	}
 }
